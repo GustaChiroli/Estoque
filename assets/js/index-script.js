4a230e4a5e6d1512692d810e431c120c918c385c
Gustavo da silva chiroli <gustavochiroli@hotmail.com>
2021-07-13 09:44:36 -0300

    Primeiro commit

---
+++ assets/js/index-script.js
@@ -0,0 +1,15 @@
+let tabela = new Vue({
+    el:'#tabela',
+    data:{
+        produtos:[
+            {id:1, nomeproduto:'leite', quantidade:50, validade:'09/06/2021', valorunit:'4.00'},
+            {id:2, nomeproduto:'waffer', quantidade:100, validade:'09/06/2021', valorunit:'2.00'},
+            {id:2, nomeproduto:'waffer', quantidade:100, validade:'09/06/2021', valorunit:'2.00'}, 
+            {id:2, nomeproduto:'waffer', quantidade:100, validade:'09/06/2021', valorunit:'2.00'}, 
+            {id:2, nomeproduto:'waffer', quantidade:100, validade:'09/06/2021', valorunit:'2.00'}, 
+            
+            
+             
+        ]
+    }
+});
